8e4deea461b8ff8402872674153a4e58197a7cf2
Mattox Beckman <mattox@illinois.edu>
2022-10-05 10:46:40 -0500

    Add lessons

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    buildInputs = [pkgs.jupyter];
+    buildInputs = [pkgs.jupyter pkgs.python310Packages.numpy pkgs.python310Packages.matplotlib pkgs.python310Packages.sympy];
 }
 
